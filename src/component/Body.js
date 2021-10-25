2ab092a5b11074b1bb0ca96c4064ed444c7bfc8a
code1511 <anch20001511@gmail.com>
2021-10-25 20:43:01 +0530

    added nodemon

---
+++ src/component/Body.js
@@ -0,0 +1,18 @@
+import '../component/title.css';
+export default function Body(){
+    return (
+        <>
+        <label >Username </label>
+        <input type="text">
+        </input>
+        <br></br>
+        <br></br>
+        <label >Password </label>
+        <input type="text">
+        </input>
+        <br></br>
+        <br></br>
+        <input type="submit"></input>
+        </>
+    );
+}
---
+++ src/component/Topone.js
@@ -0,0 +1,9 @@
+import '../component/title.css';
+export default function Topone(){
+    return(
+        <div className="Topone">
+        <h2>Hello !</h2>
+        </div>
+
+    );
+}
