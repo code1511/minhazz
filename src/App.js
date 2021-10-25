df3a0e7a05ea5dc866386cf33d8cb5eca48b66fe
code1511 <anch20001511@gmail.com>
2021-10-25 20:44:46 +0530

    added express js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Body from './component/Body';
 function App() {
   return (
     <div className="App">
      <h1>Welcome to MinhaZ</h1>
+     <Body/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './component/Topone';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import Topone from './component/Topone';
 ReactDOM.render(
   <React.StrictMode>
+    <Topone />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
